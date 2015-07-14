1e33761b71533dfe11db186266fd39a88f9b9494
Dylan Manning <dyl.manning@gmail.com>
2015-07-15 01:30:26 +1000

    reworking script/css
    - blog item back button added
    - removed cluttered attribute names
    - added normalise css

--- assets/js/boxlayout.js
+++ assets/js/boxlayout.js
@@ -10,22 +10,26 @@
  */
 var Boxlayout = (function() {
 
-  var $el = $( '#bl-main' ),
+  var $el = $( '#main' ),
     $sections = $el.children( 'section' ),
     // works section
-    $sectionWork = $( '#bl-work-section' ),
+    $sectionWork = $( '#work-section' ),
+    //blog section
+    $sectionBlog = $( '#blog-section' ),
     // work items
-    $workItems = $( '#bl-work-items > li' ),
+    $workItems = $( '#work-items > li' ),
     // work panels
-    $workPanelsContainer = $( '#bl-panel-work-items' ),
+    $workPanelsContainer = $( '#panel-work-items' ),
     $workPanels = $workPanelsContainer.children( 'div' ),
     totalWorkPanels = $workPanels.length,
     // navigating the work panels
-    $nextWorkItem = $workPanelsContainer.find( 'nav > span.bl-next-work' ),
+    $nextWorkItem = $workPanelsContainer.find( 'nav > span.next-work' ),
     // if currently navigating the work items
     isAnimating = false,
     // close work panel trigger
-    $closeWorkItem = $workPanelsContainer.find( 'nav > span.bl-icon-close' ),
+    $closeWorkItem = $workPanelsContainer.find( 'nav > span.icon-close' ),
+    // close blog item
+    $closeBlogItem = $sections.find( 'span.icon-back' ),
     transEndEventNames = {
       'WebkitTransition' : 'webkitTransitionEnd',
       'MozTransition' : 'transitionend',
@@ -52,23 +56,23 @@ var Boxlayout = (function() {
       $section.on( 'click', function() {
 
         if( !$section.data( 'open' ) ) {
-          $section.data( 'open', true ).addClass( 'bl-expand bl-expand-top' );
-          $el.addClass( 'bl-expand-item' );
+          $section.data( 'open', true ).addClass( 'expand expand-top' );
+          $el.addClass( 'expand-item' );
         }
 
-      } ).find( 'span.bl-icon-close' ).on( 'click', function() {
+      } ).find( 'span.icon-close' ).on( 'click', function() {
 
         // close the expanded section and scale up the others
-        $section.data( 'open', false ).removeClass( 'bl-expand' ).on( transEndEventName, function( event ) {
+        $section.data( 'open', false ).removeClass( 'expand' ).on( transEndEventName, function( event ) {
           if( !$( event.target ).is( 'section' ) ) return false;
-          $( this ).off( transEndEventName ).removeClass( 'bl-expand-top' );
+          $( this ).off( transEndEventName ).removeClass( 'expand-top' );
         } );
 
         if( !supportTransitions ) {
-          $section.removeClass( 'bl-expand-top' );
+          $section.removeClass( 'expand-top' );
         }
 
-        $el.removeClass( 'bl-expand-item' );
+        $el.removeClass( 'expand-item' );
 
         return false;
 
@@ -76,18 +80,27 @@ var Boxlayout = (function() {
 
     } );
 
+    $closeBlogItem.on( 'click', function( event ) {
+
+      //returns user to feed list
+      parent.history.back();
+
+      return false;
+
+    } );
+
     // clicking on a work item: the current section scales down and the respective work panel slides up
     $workItems.on( 'click', function( event ) {
 
       // scale down main section
-      $sectionWork.addClass( 'bl-scale-down' );
+      $sectionWork.addClass( 'scale-down' );
 
       // show panel for this work item
-      $workPanelsContainer.addClass( 'bl-panel-items-show' );
+      $workPanelsContainer.addClass( 'panel-items-show' );
 
       var $panel = $workPanelsContainer.find("[data-panel='" + $( this ).data( 'panel' ) + "']");
       currentWorkPanel = $panel.index();
-      $panel.addClass( 'bl-show-work' );
+      $panel.addClass( 'show-work' );
 
       return false;
 
@@ -105,18 +118,18 @@ var Boxlayout = (function() {
       currentWorkPanel = currentWorkPanel < totalWorkPanels - 1 ? currentWorkPanel + 1 : 0;
       var $nextPanel = $workPanels.eq( currentWorkPanel );
 
-      $currentPanel.removeClass( 'bl-show-work' ).addClass( 'bl-hide-current-work' ).on( transEndEventName, function( event ) {
+      $currentPanel.removeClass( 'show-work' ).addClass( 'hide-current-work' ).on( transEndEventName, function( event ) {
         if( !$( event.target ).is( 'div' ) ) return false;
-        $( this ).off( transEndEventName ).removeClass( 'bl-hide-current-work' );
+        $( this ).off( transEndEventName ).removeClass( 'hide-current-work' );
         isAnimating = false;
       } );
 
       if( !supportTransitions ) {
-        $currentPanel.removeClass( 'bl-hide-current-work' );
+        $currentPanel.removeClass( 'hide-current-work' );
         isAnimating = false;
       }
 
-      $nextPanel.addClass( 'bl-show-work' );
+      $nextPanel.addClass( 'show-work' );
 
       return false;
 
@@ -126,9 +139,9 @@ var Boxlayout = (function() {
     $closeWorkItem.on( 'click', function( event ) {
 
       // scale up main section
-      $sectionWork.removeClass( 'bl-scale-down' );
-      $workPanelsContainer.removeClass( 'bl-panel-items-show' );
-      $workPanels.eq( currentWorkPanel ).removeClass( 'bl-show-work' );
+      $sectionWork.removeClass( 'scale-down' );
+      $workPanelsContainer.removeClass( 'panel-items-show' );
+      $workPanels.eq( currentWorkPanel ).removeClass( 'show-work' );
 
       return false;
 
